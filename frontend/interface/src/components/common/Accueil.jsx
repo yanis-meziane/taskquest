da14a7a42372bafe86e1ec78ffe073a6452a0fe4
Yanis Meziane <yanismeziane04@icloud.com>
2025-06-17 14:41:56 +0200

    Avancée sur la page accueil

--- frontend/interface/src/components/common/Accueil.jsx
+++ frontend/interface/src/components/common/Accueil.jsx
@@ -1,5 +1,6 @@
 import "../CSS/commonCSS/Accueil.css"
 import taskQuestLogo from "../assets/TaskQuestLogo.jpg"
+import { Award, BookmarkCheck, ChevronRight, FlagTriangleLeft, Footprints } from "lucide-react"
 
 
 export default function Accueil(){
@@ -14,8 +15,49 @@ export default function Accueil(){
                 <article className="accueilElements">
                         <div id="secondElement">
                                 <p>
-                                    <em><b>TaskQuest</b></em> est la plateforme permettant aux personnes impliquées dans les associations de faciliter la réalisation des tâches.
+                                    <em><b>TaskQuest</b></em> est la plateforme permettant aux personnes impliquées dans les associations de faciliter la réalisation des tâches. Cette plateforme a été conçue pour rendre ludique et plus simple les activités de grandes envergures pouvant être complexes.
                                 </p>
+                        </div>
+                </article>
+
+                <article className="accueilElements">
+                    <h2>Pourquoi nous choisir ?</h2>
+                        <div className="thirdElement">
+                            <div className="argues">
+                                 <Award />
+                                <p>
+                                    Fiabilité
+                                </p>
+                            </div>
+
+                            <div className="argues">
+                                <BookmarkCheck />
+                                <p>
+                                    Efficacité
+                                </p>
+                            </div>
+
+                            <div className="argues"> 
+                                <ChevronRight />
+                                <p>
+                                    Rapidité
+                                </p>
+                            </div>
+
+                            <div className="argues">
+
+                                <FlagTriangleLeft />
+                                <p>
+                                    Simplicité
+                                </p>
+                            </div>
+
+                            <div className="argues">
+                                <Footprints />
+                                <p>
+                                    Cohérence
+                                </p>
+                            </div>
                         </div>
                 </article>
          </section>
