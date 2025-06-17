4323bdabf7e0b61341db6e47e4c12e0e78604ccc
Yanis Meziane <yanismeziane04@icloud.com>
2025-06-17 16:29:07 +0200

    Avancée sur le login

--- frontend/interface/src/components/auth/Form.jsx
+++ frontend/interface/src/components/auth/Form.jsx
@@ -1,7 +1,28 @@
+//Faire des modifications avec les API afin de retourner l'information de si l'utilisateur existe déjà ou non
 
+import "../CSS/authCSS/form.css"
 
-export default function Form(){
-    return(
-         <h1>Je suis la page pour le form</h1>
-    )
+export default function Form() {
+  function form(event) {
+    event.preventDefault();
+    alert('Formulaire envoyé')
+  }
+  return (
+
+    <section id="FormMain">
+        <h1>Login</h1>
+        <form onSubmit={form} className="formulaire">
+
+            <article>
+                <label htmlFor="firstname"> Quel est ton prénom ? </label>
+                <input name="Firstname" placeholder="Firstname..." minLength={1} /> 
+            </article>
+
+            <article>
+                <button type="submit">Search</button>
+            </article>
+        </form>
+    </section>
+
+  );
 }
