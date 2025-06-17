543d49dab2fb99dfccf69e8cf16f152bda86da5b
Yanis Meziane <yanismeziane04@icloud.com>
2025-06-17 15:32:44 +0200

    Home Page Done

--- frontend/interface/src/components/common/Accueil.jsx
+++ frontend/interface/src/components/common/Accueil.jsx
@@ -15,7 +15,7 @@ export default function Accueil(){
                 <article className="accueilElements">
                         <div id="secondElement">
                                 <p>
-                                    <em><b>TaskQuest</b></em> est la plateforme permettant aux personnes impliquées dans les associations de faciliter la réalisation des tâches. Cette plateforme a été conçue pour rendre ludique et plus simple les activités de grandes envergures pouvant être complexes.
+                                    <em><b>TaskQuest</b></em> est une plateforme OpenSource accessible sur Github permettant aux personnes impliquées dans les associations de faciliter la réalisation des tâches. Cette plateforme a été conçue pour rendre ludique et plus simple les activités de grandes envergures pouvant être complexes.
                                 </p>
                         </div>
                 </article>
@@ -58,6 +58,23 @@ export default function Accueil(){
                                     Cohérence
                                 </p>
                             </div>
+                        </div>
+                </article>
+
+                <article className="accueilElements">
+                    <h2>Comment cela fonctionne ?</h2>
+
+                        <div id="fourthElement">
+                            <p>
+                                Le concept de TaskQuest est simple et intuitif. L’application s’inspire de trois grands principes : ceux de Duolingo, Notion et Candy Crush. Dans l’interface « Mes Associations », vous retrouverez les associations auxquelles vous êtes affilié ainsi que le rôle que vous y occupez.
+                            </p>
+
+                            <p>
+                                Pour chaque association, vous accéderez aux différents événements à organiser ainsi qu’aux missions attendues. La réalisation de ces projets repose sur une décomposition en tâches concrètes, structurées sous forme de niveaux. Chaque tâche accomplie vous permet de progresser vers le niveau suivant, jusqu’au dernier niveau marquant la fin de la préparation de l’activité.
+
+                                 {/* FONCTIONNALITÉ SOUHAITÉE*/} 
+                                 {/*Les niveaux seront débloquable par pallier. C'est à dire que le président pourra définir des dates clefs que des tâches doivent faites et à partir de quel moment d'autres doivent commencer*/}
+                            </p>
                         </div>
                 </article>
          </section>
