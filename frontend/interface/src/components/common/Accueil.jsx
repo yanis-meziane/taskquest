5f64a2b87fd71374bd83472c6fbcd6a955cbbd3d
Yanis Meziane <yanismeziane04@icloud.com>
2025-06-17 12:30:20 +0200

    first commit for home page

--- frontend/interface/src/components/common/Accueil.jsx
+++ frontend/interface/src/components/common/Accueil.jsx
@@ -1,13 +1,22 @@
 import "../CSS/commonCSS/Accueil.css"
+import taskQuestLogo from "../assets/TaskQuestLogo.jpg"
+
+
 export default function Accueil(){
     return(
-         <h1 id="AccueilH1">Je suis la page d'accueil</h1>,
-
          <section className="accueilMain">
                 <article className="accueilElements">
                     <div id="firstElement">
-                        <img src="/TaskQuestLogo.jpg" alt="First_image" />
+                       <img src={taskQuestLogo} alt="First_image" id="taskQuestImage" />
                     </div>
+                </article>
+
+                <article className="accueilElements">
+                        <div id="secondElement">
+                                <p>
+                                    <em><b>TaskQuest</b></em> est la plateforme permettant aux personnes impliquées dans les associations de faciliter la réalisation des tâches.
+                                </p>
+                        </div>
                 </article>
          </section>
     )
