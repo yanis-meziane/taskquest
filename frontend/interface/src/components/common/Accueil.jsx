d406f82b1275011bdc83e0a2111d5443bab9b942
Yanis Meziane <yanismeziane04@icloud.com>
2025-06-17 10:40:25 +0200

    add image

--- frontend/interface/src/components/common/Accueil.jsx
+++ frontend/interface/src/components/common/Accueil.jsx
@@ -1,6 +1,14 @@
 import "../CSS/commonCSS/Accueil.css"
 export default function Accueil(){
     return(
-         <h1 id="AccueilH1">Je suis la page d'accueil</h1>
+         <h1 id="AccueilH1">Je suis la page d'accueil</h1>,
+
+         <section className="accueilMain">
+                <article className="accueilElements">
+                    <div id="firstElement">
+                        <img src="/TaskQuestLogo.jpg" alt="First_image" />
+                    </div>
+                </article>
+         </section>
     )
 }
