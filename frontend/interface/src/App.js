857bfe7c683f72ee53ada7ee536e57d3f5e93c18
Yanis Meziane <yanismeziane04@icloud.com>
2025-06-25 16:36:47 +0200

    Avancée sur le footer

--- frontend/interface/src/App.js
+++ frontend/interface/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-//import Contact from "./pages/Contact";
 import NavBar from "./components/common/Header"
 import Accueil from "./components/common/Accueil"
 import Form from "./components/auth/Form";
@@ -8,21 +7,23 @@ import Login from "./components/auth/Login"
 import Settings from "./components/common/Settings"
 import Footer from "./components/common/footer"
 
-//Faut rajouter et créer la page settings, mes associations et contacts
-
-  function App() {
-    return (
-      <BrowserRouter>
-      <NavBar />
-        <Routes>
-          <Route path='/' element={<Accueil />} />
-          <Route path="/form" element={<Form />} />
-          <Route path = "/login" element = {<Login />} />
-          <Route path = "/settings" element = {<Settings />} />
-        </Routes>
-      <Footer />
-      </BrowserRouter>
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="app-container">
+        <NavBar />
+        <main className="main-content">
+          <Routes>
+            <Route path='/' element={<Accueil />} />
+            <Route path="/form" element={<Form />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+}
 
 export default App;
--- frontend/interface/src/components/common/footer.jsx
+++ frontend/interface/src/components/common/footer.jsx
@@ -1,8 +1,15 @@
-
-
+import "../CSS/commonCSS/footer.css"
 
 export default function Footer(){
     return(
-        <h1>Je suis la page footer</h1>
+       <section className="footerMain">
+            <div id="footer">
+                <article className="footerElements">
+                    <p>
+                        Je suis le footer
+                    </p>
+                </article>
+            </div>
+       </section>
     )
 }
